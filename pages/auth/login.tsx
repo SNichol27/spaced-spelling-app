fd4e4cad76e26cfb7489b5d31b4bf47d19eb164b
SNichol27 <scottnichol27@aol.com>
2026-08-15 12:06:44 +0100

    Update login.tsx

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { supabase } from '@/lib/supabase';
+import Link from 'next/link';
 
 export default function Login() {
   const [email, setEmail] = useState('');
@@ -77,6 +78,7 @@ export default function Login() {
   Don't have an account?{' '}
   /auth/signup
     Sign Up
+  </Link>
 </p>
 
 </div>
