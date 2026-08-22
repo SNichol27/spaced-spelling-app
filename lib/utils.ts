d7d27ad949650b377146cbb617a43045bb2c3bde
SNichol27 <scottnichol27@aol.com>
2026-08-22 20:17:14 +0100

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,27 @@
-export const calculateReviewWeeks = (introductionWeek: number): number[] => {
+export const calculateReviewWeeks = (
+  introductionWeek: number,
+  scheduleType: string
+): number[] => {
   const reviews: number[] = [];
+
+  if (scheduleType === 'fixed8') {
+    let currentWeek = introductionWeek + 8;
+
+    while (currentWeek <= 52) {
+      reviews.push(currentWeek);
+      currentWeek += 8;
+    }
+
+    return reviews;
+  }
+
   let currentWeek = introductionWeek + 1;
-  const intervals = [1, 2, 4, 8];
+  const intervals = [1, 4, 9];
   let intervalIndex = 0;
 
   while (currentWeek <= 52) {
     reviews.push(currentWeek);
+
     if (intervalIndex < intervals.length - 1) {
       currentWeek += intervals[intervalIndex + 1];
       intervalIndex++;
